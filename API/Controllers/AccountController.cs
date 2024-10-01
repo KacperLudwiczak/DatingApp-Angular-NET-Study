d0698961bc3c63d6e5fb6781acf02600d9e98b6e
KacperLudwiczakeCERTO <casper.ludwiczak@gmail.com>
2024-10-01 15:12:02 +0200

    Add Login Endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,8 +10,9 @@ namespace API.Controllers;
 
 public class AccountController(DataContext context): BaseApiController {
 
-[HttpPost("register")] // /account/register
+[HttpPost("register")] // /api/account/register
 public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) {
+
 if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
 using var hmac = new HMACSHA512();
@@ -20,9 +22,29 @@ var user = new AppUser {
     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
     PasswordSalt = hmac.Key
 };
+
 context.Users.Add(user);
+
 await context.SaveChangesAsync();
+
 return user;
+}
+
+[HttpPost("login")] // /api/account/login
+public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) {
+    var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+    if(user == null) return Unauthorized("Invalid username");
+
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+
+    var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+    for (int i = 0; i < computedHash.Length; i++) {
+        if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+    }
+
+    return user;
 }
 private async Task<bool> UserExists(string username) {
     return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
