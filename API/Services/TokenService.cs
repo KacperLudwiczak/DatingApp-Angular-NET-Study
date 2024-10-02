88ae32598ceb73f9f1d2fa3cee04f4994abd1801
KacperLudwiczakeCERTO <casper.ludwiczak@gmail.com>
2024-10-02 11:25:27 +0200

    Create TokenService

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,12 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
-public class TokenService : ITokenService
+public class TokenService(IConfiguration config) : ITokenService
 {
     public string CreateToken(AppUser user)
     {
-        throw new NotImplementedException();
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from app settings");
+        if(tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim> 
+        {
+            new(ClaimTypes.NameIdentifier, user.UserName)
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor 
+        { 
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
