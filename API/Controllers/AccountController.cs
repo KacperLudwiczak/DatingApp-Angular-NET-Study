69e04b6b8d2fc89adffd29a0198e9f98b00e5cbd
KacperLudwiczake <casper.ludwiczak@gmail.com>
2024-10-02 11:46:32 +0200

    Create User DTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,15 +3,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context): BaseApiController 
+public class AccountController(DataContext context, ITokenService tokenService): BaseApiController 
 {
     [HttpPost("register")] // /api/account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) 
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) 
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
         
@@ -27,11 +28,15 @@ public class AccountController(DataContext context): BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
         
-        return user;
+        return new UserDto 
+        {
+            UserName = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")] // /api/account/login
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) 
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) 
     {
         var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
         
@@ -46,7 +51,11 @@ public class AccountController(DataContext context): BaseApiController
             if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
         }
 
-    return user;
+        return new UserDto 
+        {
+            UserName = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
    
     private async Task<bool> UserExists(string username) 
---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs;
+
+public class UserDto
+{
+    public required string UserName { get; set; }
+    public required string Token { get; set; }
+
+}
