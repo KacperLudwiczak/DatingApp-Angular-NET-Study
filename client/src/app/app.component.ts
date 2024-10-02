026871f2dea0ba293ecad4bc73fa1067c44f4ea9
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-02 14:19:37 +0200

    Add Logout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./nav/nav.component";
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
-  http = inject(HttpClient)
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'Dating App';
   users: any;
 
   ngOnInit(): void {
-    this.http.get("http://localhost:5000/api/users").subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Response has completed"),
-    })
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Response has completed'),
+    });
   }
 }
