c52c8b8c2b2ba54d2a079068f1514c48d60665f8
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-02 17:43:21 +0200

    Parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +12,9 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'Dating App';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -28,13 +23,5 @@ export class AppComponent implements OnInit {
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get('http://localhost:5000/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Response has completed'),
-    });
   }
 }
