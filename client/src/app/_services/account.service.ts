d5ffba977099e0fe91a4689b76815c26d97b1dc7
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-02 18:02:34 +0200

    Register a new user from frontend

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -26,4 +26,16 @@ export class AccountService {
     localStorage.removeItem('user');
     this.currentUser.set(null);
   }
+
+  register(model: any) {
+    return this.http.post<User>(`${this.baseUrl}/account/register`, model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    );
+  }
 }
