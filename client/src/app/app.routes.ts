3ebcd899747bbab9a2ee6e2bd2fd7a96fa520c65
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-03 11:29:21 +0200

    Add dummy route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,10 +7,18 @@ import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent},
-    {path: "members", component: MemberListComponent, canActivate: [authGuard]},
-    {path: "members/:id", component: MemberDetailComponent},
-    {path: "lists", component: ListsComponent},
-    {path: "messages", component: MessagesComponent},
-    {path: "**", component: HomeComponent, pathMatch: "full"},
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
+  },
+
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
