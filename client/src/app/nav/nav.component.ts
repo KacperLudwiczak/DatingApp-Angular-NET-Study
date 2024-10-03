e9e1ba83354529c4d3f93ffeb2e4c6bd90fa7919
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-03 12:12:21 +0200

    Add new theme

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,14 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
+  imports: [
+    FormsModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    TitleCasePipe,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
@@ -23,7 +30,7 @@ export class NavComponent {
       next: () => {
         this.router.navigateByUrl('/members');
       },
-      error: (error) => this.toastr.error(error.error)
+      error: (error) => this.toastr.error(error.error),
     });
   }
 
