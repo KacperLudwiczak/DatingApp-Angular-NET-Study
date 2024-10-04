3237b379feeba44f700bff5a031e478e0ace5094
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-04 17:08:20 +0200

    Create a repository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace API;
+public class UserRepository(DataContext context) : IUserRepository
+{
+    public async Task<AppUser?> GetUserByIdAsync(int id)
+    {
+        return await context.Users.FindAsync(id);
+    }
+    public async Task<AppUser?> GetUserByUsernameAsync(string username)
+    {
+        return await context.Users
+            .SingleOrDefaultAsync(x => x.UserName == username);
+    }
+    public async Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+        return await context.Users
+            .ToListAsync();
+    }
+    public async Task<bool> SaveAllAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+    public void Update(AppUser user)
+    {
+        context.Entry(user).State = EntityState.Modified;
+    }
+}
