1b4d65209151045702b76fb16bcbd84588faa5e1
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-06 20:17:20 +0200

    Delete photo in backend

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -70,4 +70,21 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
         if (await userRepository.SaveAllAsync()) return NoContent();
         return BadRequest("Problem setting main photo");
     }
+
+    [HttpDelete("delete-photo/{photoId:int}")] // /api/users/delete-photo/photoId
+    public async Task<ActionResult> DeletePhoto(int photoId)
+    {
+        var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+        if (user == null) return BadRequest("User not found");
+        var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+        if (photo == null || photo.IsMain) return BadRequest("This photo cannot be deleted");
+        if (photo.PublicId != null)
+        {
+            var result = await photoService.DeletePhotoAsync(photo.PublicId);
+            if (result.Error != null) return BadRequest(result.Error.Message);
+        }
+        user.Photos.Remove(photo);
+        if (await userRepository.SaveAllAsync()) return Ok();
+        return BadRequest("Problem deleting photo");
+    }
 }
