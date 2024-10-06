2eeb99e1e29e2d99671a4da82d98280ee720c70c
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-06 14:04:14 +0200

    Update user in frontend

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -36,8 +36,11 @@ export class MemberEditComponent implements OnInit {
     });
   }
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated successfully');
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: () => {
+        this.toastr.success('Profile updated successfully');
+        this.editForm?.reset(this.member);
+      }
+    })
   }
 }
