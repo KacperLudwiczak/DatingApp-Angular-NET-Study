37b651a0a0188326dd28b141d36d38d09add45f6
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-08 16:05:42 +0200

    Add filter buttons

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -5,11 +5,12 @@ import { MemberCardComponent } from '../member-card/member-card.component';
 import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccountService } from '../../_services/account.service';
 import { UserParams } from '../../_models/userParams';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
@@ -17,12 +18,19 @@ export class MemberListComponent implements OnInit {
   memberService = inject(MembersService);
   private accountService = inject(AccountService);
   userParams = new UserParams(this.accountService.currentUser());
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}]
 
   ngOnInit(): void {
     if (!this.memberService.paginatedResult()) this.loadMembers();
   }
+
   loadMembers() {
     this.memberService.getMembers(this.userParams);
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
