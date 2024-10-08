d2df356b80b34e465d6fdfc1a86bb649b1efc76f
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-08 15:03:41 +0200

    Add filtering to backend

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -15,6 +15,7 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
+        userParams.CurrentUsername = User.GetUsername();
         var users = await userRepository.GetMembersAsync(userParams);
         Response.AddPaginationHeader(users);
         return Ok(users);
