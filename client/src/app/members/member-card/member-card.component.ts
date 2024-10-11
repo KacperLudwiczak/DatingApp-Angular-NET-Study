6a0869aacb184d57f738f69965c456a876dc41a9
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-11 19:03:13 +0200

    Add toggle functionality

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -14,4 +14,16 @@ export class MemberCardComponent {
   private likeService = inject(LikesService);
   member = input.required<Member>();
   hasLiked = computed(() => this.likeService.likeIds().includes(this.member().id));
+
+  toggleLike() {
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likeService.likeIds.update(ids => ids.filter(x => x !== this.member().id))
+        } else {
+          this.likeService.likeIds.update(ids => [...ids, this.member().id])
+        }
+      }
+    })
+  }
 }
