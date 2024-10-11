5d069ea56f5d2b13579a63928ea89a6784d22343
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-11 14:59:09 +0200

    Add likes repository

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,50 @@
+using API.Data;
+using API.DTOs;
+using API.Helpers;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+namespace API;
+
+public class LikesRepository(DataContext context, IMapper mapper) : ILikesRepository
+{
+    public void AddLike(UserLike like)
+    {
+        context.Likes.Add(like);
+    }
+    public void DeleteLike(UserLike like)
+    {
+        context.Likes.Remove(like);
+    }
+    public async Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId)
+    {
+        return await context.Likes
+            .Where(x => x.SourceUserId == currentUserId)
+            .Select(x => x.TargetUserId)
+            .ToListAsync();
+    }
+    public async Task<UserLike?> GetUserLike(int sourceUserId, int targetUserId)
+    {
+        return await context.Likes.FindAsync(sourceUserId, targetUserId);
+    }
+    public async Task<IEnumerable<MemberDto>> GetUserLikes(string predicate, int userId)
+    {
+        var likes = context.Likes.AsQueryable();
+        switch (predicate)
+        {
+            case "liked":
+                return await likes.Where(x => x.SourceUserId == userId).Select(x => x.TargetUser).ProjectTo<MemberDto>(mapper.ConfigurationProvider).ToListAsync();
+            case "likedBy":
+                return await likes.Where(x => x.TargetUserId == userId).Select(x => x.SourceUser).ProjectTo<MemberDto>(mapper.ConfigurationProvider).ToListAsync();
+            default:
+                var likeIds = await GetCurrentUserLikeIds(userId);
+                return await likes.Where(x => x.TargetUserId == userId && likeIds.Contains(x.SourceUserId)).Select(x => x.SourceUser).ProjectTo<MemberDto>(mapper.ConfigurationProvider).ToListAsync();
+        }
+
+    }
+
+    public async Task<bool> SaveChanges()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
