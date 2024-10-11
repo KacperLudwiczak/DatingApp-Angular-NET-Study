1e5c1ceaa6d3445cfccca49526c225ec2524fecd
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-11 11:48:57 +0200

    Sorting in frontend

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,15 +2,16 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../member-card/member-card.component';
-import { PageChangedEvent, PaginationModule } from 'ngx-bootstrap/pagination';
+import {  PaginationModule } from 'ngx-bootstrap/pagination';
 import { AccountService } from '../../_services/account.service';
 import { UserParams } from '../../_models/userParams';
 import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule, FormsModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule, ButtonsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
