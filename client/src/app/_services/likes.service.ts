35f8233343699f1a447c983f535dd9421ef3f445
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-11 19:25:08 +0200

    Add likes component

--- client/src/app/_services/likes.service.ts
+++ client/src/app/_services/likes.service.ts
@@ -18,8 +18,8 @@ export class LikesService {
     return this.http.post(`${this.baseUrl}/likes/${targetId}`, {})
   }
 
-  getLikes(predicate: string, pageNumber: number, pageSize: number) {
-    return this.http.get(`${this.baseUrl}/likes?predicate=${predicate}`)
+  getLikes(predicate: string) {
+    return this.http.get<Member[]>(`${this.baseUrl}/likes?predicate=${predicate}`)
   }
   getLikeIds() {
     return this.http.get<number[]>(`${this.baseUrl}/likes/list`).subscribe({
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LikesService } from '../_services/likes.service';
+import { Member } from '../_models/member';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent, ButtonsModule, FormsModule],
   templateUrl: './lists.component.html',
-  styleUrl: './lists.component.css'
+  styleUrl: './lists.component.css',
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit {
+  private likesService = inject(LikesService);
+  members: Member[] = [];
+  predicate = 'liked';
 
+  ngOnInit(): void {
+    this.loadLikes(); 
+  }
+
+  getTitle() {
+    switch (this.predicate) {
+      case 'liked': return 'Members You Like';
+      case 'likedBy': return 'Members Who Like You';
+      default: return 'Mutual'
+    }
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members = members
+    })
+  }
 }
