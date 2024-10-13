787cd71e5f2f4bef7a5e5bdfe987c2c21a9181b5
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-13 17:21:08 +0200

    Add message migrations

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,12 +7,13 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         builder.Entity<UserLike>()
-            .HasKey(key => new {key.SourceUserId, key.TargetUserId});
+            .HasKey(key => new { key.SourceUserId, key.TargetUserId });
         builder.Entity<UserLike>()
             .HasOne(source => source.SourceUser)
             .WithMany(liked => liked.LikedUsersByOurUser)
@@ -23,5 +24,13 @@ public class DataContext(DbContextOptions options) : DbContext(options)
             .WithMany(liked => liked.UsersWhoLikedOurUser)
             .HasForeignKey(source => source.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+        builder.Entity<Message>()
+            .HasOne(x => x.Recipient)
+            .WithMany(x => x.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.Entity<Message>()
+            .HasOne(x => x.Sender)
+            .WithMany(x => x.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
