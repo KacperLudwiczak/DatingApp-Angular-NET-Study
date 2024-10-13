d48ce82e919529dba198aa59f1c077c79a217a86
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-13 19:42:51 +0200

    Add message service

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
   imports: [],
   templateUrl: './messages.component.html',
-  styleUrl: './messages.component.css'
+  styleUrl: './messages.component.css',
 })
-export class MessagesComponent {
 
+export class MessagesComponent  implements OnInit {
+   messageService = inject(MessageService);
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 6;
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
