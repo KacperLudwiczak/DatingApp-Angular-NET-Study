9e4e27b42e593763ef1d319d57198dd41b408210
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-14 12:47:54 +0200

    Configuring IdentityServiceExtensions

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 
@@ -9,6 +12,14 @@ public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddIdentityCore<AppUser>(options =>
+       {
+           options.Password.RequireNonAlphanumeric = false;
+       })
+         .AddRoles<AppRole>()
+         .AddRoleManager<RoleManager<AppRole>>()
+         .AddEntityFrameworkStores<DataContext>();
+
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
         {
             var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey not found");
