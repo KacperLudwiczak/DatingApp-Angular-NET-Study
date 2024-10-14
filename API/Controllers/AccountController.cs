e044b7b919706a0c08d784fd206e608c6faaf59c
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-14 14:33:39 +0200

    Add roles to jwt token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,7 +29,7 @@ public class AccountController(UserManager<AppUser> userManager, ITokenService t
         return new UserDto
         {
             UserName = user.UserName,
-            Token = tokenService.CreateToken(user),
+            Token = await tokenService.CreateToken(user),
             KnownAs = user.KnownAs,
             Gender = user.Gender
         };
@@ -51,7 +51,7 @@ public class AccountController(UserManager<AppUser> userManager, ITokenService t
         {
             UserName = user.UserName,
             KnownAs = user.KnownAs,
-            Token = tokenService.CreateToken(user),
+            Token = await tokenService.CreateToken(user),
             Gender = user.Gender,
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -3,13 +3,14 @@ using System.Security.Claims;
 using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
-public class TokenService(IConfiguration config) : ITokenService
+public class TokenService(IConfiguration config, UserManager<AppUser> userManager) : ITokenService
 {
-    public string CreateToken(AppUser user)
+    public async Task<string> CreateToken(AppUser user)
     {
         var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from app settings");
         if (tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
@@ -22,6 +23,9 @@ public class TokenService(IConfiguration config) : ITokenService
             new(ClaimTypes.Name, user.UserName)
         };
 
+        var roles = await userManager.GetRolesAsync(user);
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+        
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
         var tokenDescriptor = new SecurityTokenDescriptor
