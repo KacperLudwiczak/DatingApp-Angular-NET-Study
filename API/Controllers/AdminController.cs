4223132303a7dc16122f34261c7b8b2b04c1fd10
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-14 15:08:03 +0200

    Edit user roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -13,7 +13,7 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
     {
         var users = await userManager.Users
             .OrderBy(x => x.UserName)
-            .Select(x => new 
+            .Select(x => new
             {
                 x.Id,
                 Username = x.UserName,
@@ -21,7 +21,23 @@ public class AdminController(UserManager<AppUser> userManager) : BaseApiControll
             }).ToListAsync();
         return Ok(users);
     }
-    
+
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpPost("edit-roles/{username}")]
+    public async Task<ActionResult> EditRoles(string username, string roles)
+    {
+        if (string.IsNullOrEmpty(roles)) return BadRequest("you must select at least one role");
+        var selectedRoles = roles.Split(",").ToArray();
+        var user = await userManager.FindByNameAsync(username);
+        if (user == null) return BadRequest("User not found");
+        var userRoles = await userManager.GetRolesAsync(user);
+        var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+        if (!result.Succeeded) return BadRequest("Failed to add to roles");
+        result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+        if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+        return Ok(await userManager.GetRolesAsync(user));
+    }
+
     [Authorize(Policy = "ModeratePhotoRole")]
     [HttpGet("photos-to-moderate")]
     public ActionResult GetPhotosForModeration()
