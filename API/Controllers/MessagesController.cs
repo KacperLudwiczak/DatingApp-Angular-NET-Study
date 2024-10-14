351f84789629acf43a2f49c9d549671ed89e17dd
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-14 12:13:14 +0200

    Delete message in backend

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API;
 
 [Authorize]
-public class MessagesController(IMessageRepository messageRepository, IUserRepository userRepository, 
+public class MessagesController(IMessageRepository messageRepository, IUserRepository userRepository,
     IMapper mapper) : BaseApiController
 {
     [HttpPost]
@@ -17,7 +17,7 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
         var username = User.GetUsername();
         if (username == createMessageDto.RecipientUsername.ToLower())
             return BadRequest("You cannot message yourself");
-        
+
         var sender = await userRepository.GetUserByUsernameAsync(username);
         var recipient = await userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
         if (recipient == null || sender == null) return BadRequest("Cannot send message at this time");
@@ -36,35 +36,36 @@ public class MessagesController(IMessageRepository messageRepository, IUserRepos
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser(
-        [FromQuery]MessageParams messageParams)
+        [FromQuery] MessageParams messageParams)
     {
         messageParams.Username = User.GetUsername();
         var messages = await messageRepository.GetMessagesForUser(messageParams);
         Response.AddPaginationHeader(messages);
         return messages;
     }
-    
+
     [HttpGet("thread/{username}")]
     public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
     {
         var currentUsername = User.GetUsername();
         return Ok(await messageRepository.GetMessageThread(currentUsername, username));
     }
-    
-    // [HttpDelete("{id}")]
-    // public async Task<ActionResult> DeleteMessage(int id)
-    // {
-    //     var username = User.GetUsername();
-    //     var message = await messageRepository.GetMessage(id);
-    //     if (message == null) return BadRequest("Cannot delete this message");
-    //     if (message.SenderUsername != username && message.RecipientUsername != username) 
-    //         return Forbid();
-    //     if (message.SenderUsername == username) message.SenderDeleted = true;
-    //     if (message.RecipientUsername == username) message.RecipientDeleted = true;
-    //     if (message is {SenderDeleted: true, RecipientDeleted: true}) {
-    //         messageRepository.DeleteMessage(message);
-    //     }
-    //     if (await messageRepository.SaveAllAsync()) return Ok();
-    //     return BadRequest("Problem deleting the message");
-    // }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteMessage(int id)
+    {
+        var username = User.GetUsername();
+        var message = await messageRepository.GetMessage(id);
+        if (message == null) return BadRequest("Cannot delete this message");
+        if (message.SenderUsername != username && message.RecipientUsername != username)
+            return Forbid();
+        if (message.SenderUsername == username) message.SenderDeleted = true;
+        if (message.RecipientUsername == username) message.RecipientDeleted = true;
+        if (message is { SenderDeleted: true, RecipientDeleted: true })
+        {
+            messageRepository.DeleteMessage(message);
+        }
+        if (await messageRepository.SaveAllAsync()) return Ok();
+        return BadRequest("Problem deleting the message");
+    }
 }
