4503b0537cbae8e8264d107eae8473019acbb9e2
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-15 12:46:39 +0200

    Authenticating to SignalR

--- API/Program.cs
+++ API/Program.cs
@@ -17,7 +17,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
